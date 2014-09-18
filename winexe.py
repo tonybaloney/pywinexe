1a72c4a0996627fb7b18d813f5c93bb386208f94
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2014-09-18 10:36:49 +0200

    Logging in main

--- winexe.py
+++ winexe.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 import argparse
 import winexe
+import sys
+import logging
 
 from winexe.exceptions import RequestException
+
+def setup_logging():
+    log = logging.getLogger('winexe')
+    log.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    log.addHandler(handler)
 
 def main():
     parser = argparse.ArgumentParser(description='Run scripts on remote windows.')
@@ -10,36 +21,34 @@ def main():
     parser.add_argument('-u', '--user', required=True)
     parser.add_argument('-p', '--password')
     parser.add_argument('-i', '--host', required=True)
-
-    subparsers = parser.add_subparsers(help='commands')
+    parser.add_argument('-v', '--verbose')
+    
+    subparsers = parser.add_subparsers(help='commands', dest='method')
 
     cmd_parser = subparsers.add_parser('cmd', help='Run command with cmd.exe')
     cmd_parser.add_argument('cmd')
 
     ps_parser = subparsers.add_parser('ps', help='Run command with powershell')
-    ps_parser.add_argument('ps')
+    ps_parser.add_argument('cmd')
 
     file_parser = subparsers.add_parser('file', help='Run file')
-    file_parser.add_argument('file', type=argparse.FileType('r'))
+    file_parser.add_argument('cmd', type=argparse.FileType('r'))
 
     args = parser.parse_args()
-    args_dict = vars(args)
 
-    method = None
-
-    if args_dict.get('cmd'):
-        method = 'cmd'
-    elif args_dict.get('ps'):
-        method = 'ps'
-    elif args_dict.get('file'):
-        method = 'file'
-    else:
-        raise Exception('Should not happen!')
+    if args.verbose:
+        setup_logging()
 
     try:
-        print winexe.run(method, **args_dict)
+        print(winexe.run(args.method,
+            cmd=args.cmd,
+            user=args.user,
+            password=args.password,
+            host=args.host
+        ))
     except RequestException, e:
-        print str(e)
+        print('Error calling: %s' % e.request, file=sys.stderr)
+        print(str(e))
 
 if __name__ == '__main__':
     main()
